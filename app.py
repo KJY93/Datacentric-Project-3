cde435a6b13879ecf33b2f2564d572f4890b5969
KJY93 <38803618+KJY93@users.noreply.github.com>
2019-12-28 22:48:43 +0800

    added route to handle AJAX query to retrieve cereals nutritional info

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def index():
     cursor = mysql.connection.cursor()
 
     cursor.execute(('''SELECT Cereals.name, manufacturer_description, Type.cereals_type,
-            Cereals.protein, Cereals.fat, Cereals.sodium, Cereals.fiber, Cereals.carbohyrates, Cereals.sugars,
+            Cereals.protein, Cereals.fat, Cereals.sodium, Cereals.fiber, Cereals.carbohydrates, Cereals.sugars,
             Cereals.potassium, Cereals.vitamins, Cereals.ratings FROM Cereals INNER JOIN Manufacturer ON Cereals.manufacturer_id =
             Manufacturer.manufacturer_id INNER JOIN Type ON Cereals.type_id = Type.type_id ORDER BY Cereals.ratings DESC'''))
 
@@ -67,12 +67,25 @@ def index():
     
     for j in manufacturer_category_description:
         manufacturer_category_description_array.append(j['manufacturer_description'])
-    
-    print(cereal_record_count)
+
     return render_template('index.html', cereal_menu_item=cereal_menu_item, manufacturer_category_description_array=manufacturer_category_description_array, col_headers=col_headers, cereals_menu=cereals_menu, cereal_item_manufacturer_count=cereal_item_manufacturer_count, cereal_record_count=cereal_record_count)
     
     # else:
     # return 'YAY'
+    
+@app.route('/query', methods=["GET"])
+def query():
+    
+    item_selected = request.args.get('item')
+
+    # Query for all the menu items in the Menu table
+    cursor = mysql.connection.cursor()
+
+    cursor.execute('''SELECT calories, protein, fat, sodium, fiber, carbohydrates, sugars, potassium, vitamins FROM Cereals WHERE name=''' + "\'" + item_selected + "\'")
+    
+    cereal = cursor.fetchall();
+    
+    return jsonify({"cereal":cereal})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
